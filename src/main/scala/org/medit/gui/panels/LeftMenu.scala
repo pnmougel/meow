9eb3b4385c128b93f117ccfe0ee4ea89b334d775
pnmougel <pnmougel@gmail.com>
2015-09-24 23:13:21 +0200

    Removed the create entry option

--- src/main/scala/org/medit/gui/panels/LeftMenu.scala
+++ src/main/scala/org/medit/gui/panels/LeftMenu.scala
@@ -35,7 +35,7 @@ object LeftMenu extends WebPanel(new BorderLayout) {
 
   menuContent.setMargin(0, 10, 0, 10)
   menuContent.addComponent(tasksLabel)
-  menuContent.addComponent(addAppLabel)
+//  menuContent.addComponent(addAppLabel)
 //  menuContent.addComponent(new TaskPanel("Create an application folder", new AddFolderPanel()))
   menuContent.addComponent(new TaskPanel("Filter the applications", FiltersPanel))
 
